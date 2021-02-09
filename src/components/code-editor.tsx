5ad42bdd198bfa9efb50ee8b241c42e409ccc66a
Bill <wmoore98@sbcglobal.net>
2021-02-08 22:29:38 -0500

    Lesson 131 - add bulma css formatting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import "./code-editor.css";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
 import parser from "prettier/parser-babel";
@@ -25,19 +26,26 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
 
   const onClickFormat = () => {
     const unformatted = editorRef.current.getModel().getValue();
-    const formatted = prettier.format(unformatted, {
-      parser: "babel",
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      // singleQuote: true,
-    });
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        // singleQuote: true,
+      })
+      .replace(/\n$/, "");
     editorRef.current.setValue(formatted);
   };
 
   return (
-    <div>
-      <button onClick={onClickFormat}>Format</button>
+    <div className='editor-wrapper'>
+      <button
+        className='button button-format is-primary is-small'
+        onClick={onClickFormat}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
